199e782a3d22e2e9f3019358f3e7b945c3d3f6e9
jprice628 <jprice628@hotmail.com>
2015-03-24 15:34:13 -0700

    Fixed player class.

---
+++ LittleLeague/Domain/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Player
+    {
+        public int PlayerId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public bool IsRegistered { get; private set; }
+
+        public Player()
+        {
+        }
+
+        public Player(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void Register()
+        {
+            IsRegistered = true;
+        }
+
+        public void UnRegister()
+        {
+            IsRegistered = false;
+        }
+    }
+}
