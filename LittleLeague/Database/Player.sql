ba7ae4dff4db74de2fb17c2dfb4e73ecfee69f4c
jprice628 <jprice628@hotmail.com>
2015-03-24 15:27:22 -0700

    Revert "Added Player entity."
    This reverts commit 87ccc9e29a6f6f11933840c5c8fedd52d68be9fa.

--- LittleLeague/Database/Player.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Player]
-(
-	[PlayerId] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [FirstName] VARCHAR(50) NOT NULL, 
-    [LastName] VARCHAR(50) NOT NULL, 
-    [IsRegistered] BIT NOT NULL
-)
